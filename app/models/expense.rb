2ca16366a95837f4e9f72d59e7045f0f1daca5d0
102306093 <102306093@nccu.edu.tw>
2015-06-09 12:24:26 +0800

    modify dashboard

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,7 @@ class Expense < ActiveRecord::Base
 	belongs_to :user
 
 	def self.dashboard(u_id)
-		data=Expense.select("kind,sum(money) as total,user_id").where(:user_id => u_id).group("kind")
+		data=Expense.select("kind,sum(money) as total").where(user_id: u_id).group("kind")
 		data.map{|d|[d.kind,d.total]}
 	end
 end
